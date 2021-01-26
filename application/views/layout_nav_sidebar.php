8e46f8930cb300bf5e0992dd71858d1f403694c9
rivaldi-fsociety <rivaldiir10@gmail.com>
2021-01-27 00:01:30 +0700

    Update layout_nav_sidebar.php
    Pembaruan Nav Sidebar

--- application/views/layout_nav_sidebar.php
+++ application/views/layout_nav_sidebar.php
@@ -45,7 +45,39 @@
                   <li class="<?php if($page== 'lap_inventory'){echo'active';}?>"><a class="nav-link" href="<?= base_url('laporan_inventory')?>">Inventory</a></li>
                   <li class="<?php if($page== 'lap_finance'){echo'active';}?>"><a class="nav-link" href="<?= base_url('laporan_finance')?>">Finance</a></li>
                 </ul>
+              <li class="menu-header">Akuntansi</li>
+              <li class="nav-item dropdown <?php if($drop== 'saldo_awal'){echo'active';}?>">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-file-alt"></i><span>Saldo Awal</span></a>
+                <ul class="dropdown-menu">
+                  <li class="<?php if($page== 'Saldo_awal'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>">Saldo Awal</a></li>
+                  <li class="<?php if($page== 'Saldo_awal_hutang'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>">Saldo Awal Hutang</a></li>
+                  <li class="<?php if($page== 'Saldo_awal_piutang'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>">Saldo Awal Piutang</a></li>
+                  <li class="<?php if($page== 'Saldo_awal_intentaris'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>">Saldo Awal Inventaris</a></li>
+                  <li class="<?php if($page== 'Saldo_awal_persediaan'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>">Saldo Awal Persediaan</a></li>
+                </ul>
+              <li class="<?php if($page== 'Setup_jurnal'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>"><i class="far fa-address-card"></i> <span>Setup Jurnal</span></a></li>  
+              <li class="nav-item dropdown <?php if($drop== 'utang_dan_piutang'){echo'active';}?>">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-file-alt"></i><span>Utang Dan Piutang</span></a>
+                <ul class="dropdown-menu">
+                  <li class="<?php if($page== 'Saldo_awal'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>">Utang Usaha</a></li>
+                  <li class="<?php if($page== 'Saldo_awal_hutang'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>">Piutang Usaha</a></li>
+                </ul>
               </li>
+              <li class="<?php if($page== 'Jurnal_umum'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>"><i class="far fa-address-card"></i> <span>Jurnal Umum</span></a></li>
+              <li class="<?php if($page== 'Buku_besar'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>"><i class="far fa-address-card"></i> <span>Buku Besar</span></a></li> 
+              <li class="<?php if($page== 'Neraca_saldo'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>"><i class="far fa-address-card"></i> <span>Neraca Saldo</span></a></li>
+              <li class="<?php if($page== 'Jurnal_penyesuaian'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>"><i class="far fa-address-card"></i> <span>Jurnal Penyesuaian</span></a></li>
+              <li class="<?php if($page== 'Laba_rugi'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>"><i class="far fa-address-card"></i> <span>Laba Rugi</span></a></li>
+              <li class="<?php if($page== 'Perubahan_modal'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>"><i class="far fa-address-card"></i> <span>Perubahan Modal</span></a></li>
+              <li class="<?php if($page== 'Neraca'){echo'active';}?>"><a class="nav-link" href="<?= base_url('#')?>"><i class="far fa-address-card"></i> <span>Neraca</span></a></li>             
+
+
+
+
+
+
+
+
               <li class="menu-header">KELOLA USER</li>
               <li class="<?php if($page== 'user'){echo'active';}?>"><a class="nav-link" href="<?= base_url('user')?>"><i class="fas fa-user-cog"></i> <span>User</span></a></li>
             <!--   <li class="menu-header">Laporan</li>
@@ -53,7 +85,6 @@
                -->
         </aside>
       </div>
-
 <script>
     var hoursLabel = document.getElementById("hours");
     var minutesLabel = document.getElementById("minutes");
